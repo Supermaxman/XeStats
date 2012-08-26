40f82cf4c732679089051dcc6a29671c79252cad
Rainbow <tehrainbowguy@gmail.com>
2012-08-26 21:12:24 +0100

    Comment

--- XeStats/src/me/supermaxman/xestats/XeStats.java
+++ XeStats/src/me/supermaxman/xestats/XeStats.java
@@ -1,14 +1,12 @@
 package me.supermaxman.xestats;
 
-import java.util.logging.Logger;
-
 import me.supermaxman.xestats.executors.StatsMainExecutor;
 import net.milkbowl.vault.chat.Chat;
-import net.milkbowl.vault.permission.Permission;
-
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public class XeStats extends JavaPlugin {
 
@@ -17,23 +15,23 @@ public class XeStats extends JavaPlugin {
     public static FileConfiguration conf;
 
     private final XeStatsListener Listener = new XeStatsListener(this);
-    public static Permission permission = null;
+    //    public static Permission permission = null;
     public static Chat chat = null;
     public static XeStats XE;
 
     @Override
     public void onDisable() {
         log.info("Disabled.");
-        
+
     }
-    
-    
+
+
     @Override
     public void onEnable() {
         XE = this;
         log = getLogger();
         conf = getConfig();
-        if (!setupPermissions() || !setupChat()) {
+        if (!setupChat()) {
             log.severe("Vault fail!");
             this.setEnabled(false);
             return;
@@ -44,26 +42,22 @@ public class XeStats extends JavaPlugin {
 
         getCommand("stats").setExecutor(new StatsMainExecutor(this));
     }
-    
+
 
     void setupConfig() {
-        //if (conf.get("worldinchat") == null) {
-            //conf.set("worldinchat", false);
-        //}
-        
         saveConfig();
     }
-    
-    private boolean setupPermissions() {
-        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
-        if (permissionProvider != null) {
-            permission = permissionProvider.getProvider();
-            return true;
-        }
-        return false;
-    }
-    
-    
+
+//    private boolean setupPermissions() {
+//        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+//        if (permissionProvider != null) {
+//            permission = permissionProvider.getProvider();
+//            return true;
+//        }
+//        return false;
+//    }
+//
+
     private boolean setupChat() {
         RegisteredServiceProvider<Chat> chatRegisteredServiceProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
         if (chatRegisteredServiceProvider != null) {
